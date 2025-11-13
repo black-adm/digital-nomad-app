7d50ec98eece7bc4d99889b45973a80e2ca4f17d
black-adm <mattheuscontato17@gmail.com>
2025-11-13 00:28:14 -0300

    fix: clean project

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -8,7 +8,9 @@ export default function ProtectedLayout() {
   }
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{ headerShown: false, fullScreenGestureEnabled: true }}
+    >
       <Stack.Screen name="(tab)" />
     </Stack>
   );
